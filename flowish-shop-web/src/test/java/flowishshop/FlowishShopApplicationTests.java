45a759cdecb9bea894d5656e9f7c795a87cfb204
leartallushaj <leartallushaj14@gmail.com>
2019-11-21 16:37:30 +0100

    adding index page and controller. closes #9

--- flowish-shop-web/src/test/java/flowishshop/FlowishShopApplicationTests.java
+++ flowish-shop-web/src/test/java/flowishshop/FlowishShopApplicationTests.java
@@ -1,13 +1,3 @@
 package flowishshop;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FlowishShopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
