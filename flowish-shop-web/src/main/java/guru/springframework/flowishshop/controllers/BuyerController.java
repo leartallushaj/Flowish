51e251bdb2147b8dbb50498d3344d90a85a64d3e
leartallushaj <leartallushaj14@gmail.com>
2019-11-21 17:35:41 +0100

    adding buyer index page and controller. closes #14

--- flowish-shop-web/src/main/java/guru/springframework/flowishshop/controllers/BuyerController.java
+++ flowish-shop-web/src/main/java/guru/springframework/flowishshop/controllers/BuyerController.java
@@ -1,9 +1,14 @@
 package guru.springframework.flowishshop.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BuyerController {
 
+    @RequestMapping({"/buyers","/buyers/index","/buyers/index.html"})
+    public String listBuyers(){
+        return "buyers/index";
+    }
 
 }
